2bfd9fafc340fbb4f7bd6de54997adf81fc32915
User <marcos.vmlopes@gmail.com>
2020-01-31 14:46:26 -0300

    ajustes necessarios

--- features/support/hooks.rb
+++ features/support/hooks.rb
@@ -3,7 +3,8 @@ Before do
   @movie_page = MoviePage.new
   @sidebar = SideBarView.new
 
-  page.current_window.resize_to(1920, 1080)
+  page.driver.browser.manage.window.maximize
+  #page.current_window.resize_to(1920, 1080)
 end
 
 Before("@login") do
@@ -12,3 +13,10 @@ Before("@login") do
   @login_page.with(user["email"], user["pass"])
 end
 
+After do |scenario|
+  #if scenario.failed?
+  temp_shot = page.save_screenshot("log/temp_shot.png")
+  screenshot = Base64.encode64(File.open(temp_shot).read)
+  embed(screenshot, "image/png", "Screenshot")
+  #end
+end
